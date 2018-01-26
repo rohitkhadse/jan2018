12173ec5d3d572c5698a1f0cec772828b55910c0
rohitkhadse <rohit.a.khadse@gmail.com>
2018-01-25 23:59:37 -0600

    Use of hashCode() and equals()

---
+++ KsqaureIT/src/com/hasmap/SortingIntArray.java
@@ -0,0 +1,23 @@
+package com.hasmap;
+
+import java.util.Arrays;
+
+public class SortingIntArray {
+
+	public static void main(String[] args) {
+		int[] array = new int[100];
+		
+		for(int i=0;i<array.length;i++){
+			array[i]=(int) (Math.random()*100);
+		}
+		
+		Arrays.sort(array);
+		
+		for(int a:array){
+			System.out.println(a);
+		}
+		
+
+	}
+
+}
